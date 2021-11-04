29f85ff6b726c43cb9302fc7a5913cff3360a726
Kr3b5 <codersuniverse42@gmail.com>
2021-11-04 13:31:49 +0100

    Refactor Input to Iterator

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -1,24 +1,39 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.io.*;
+import java.util.Iterator;
 
-public class Input {
 
-    private FileInputStream fis;
+public class Input implements Iterator<Character> {
 
-    public Input(File f){
-        try {
-            fis = new FileInputStream(f);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+    /** The logger for this class. */
+    private static final Logger logger = LogManager.getLogger(Input.class.getName());
+
+    private static final int EOF    = -1;
+    private static final int ERROR  = -2;
+
+    private final BufferedReader reader;
+    private int buffer;
+
+    public Input(final String filename) throws FileNotFoundException {
+        this.reader = new BufferedReader(new FileReader(filename));
+        next(); //read first char in buffer
     }
 
-    public char next(){
+    @Override
+    public Character next() {
+        int out = buffer;
         try {
-            return (char)fis.read();
+            buffer = reader.read();
         } catch (IOException e) {
-            e.printStackTrace();
+            buffer = ERROR;
+            logger.error(e.getMessage());
         }
-        return 0;
+        return (char) out;
     }
 
+    @Override
+    public boolean hasNext() {
+        return buffer != EOF && buffer != ERROR;
+    }
 }
