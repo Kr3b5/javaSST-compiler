13576995f2ffb904c7f913a97f833a16640c03f6
Kr3b5 <codersuniverse42@gmail.com>
2021-11-06 15:17:17 +0100

    Refactor testfile more symbols

--- src/test/java/Testfiles/HelloWorld.java
+++ src/test/java/Testfiles/HelloWorld.java
@@ -1,9 +1,26 @@
 package Testfiles;
 
 /* HelloWorld.java */
+@SuppressWarnings("ALL")
 public class HelloWorld
 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int x = 2 + 3 - 1;
+        int y = 5 * 7;
+        double z = 3 / 2;
+
+        if( x <= y ){
+            System.out.println("1");
+        }
+        if( z >= y ){
+            System.out.println("2");
+        }
+        if( x != z ){
+            System.out.println("3,5");
+        }else if( x == y ){
+            System.out.println("4");
+        }
     }
 }
