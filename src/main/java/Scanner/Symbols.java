5a2d44f72c35b4f6f2a4d4113a72e54db754fc5a
Kr3b5 <codersuniverse42@gmail.com>
2021-11-06 14:01:12 +0100

    complete symbols

--- src/main/java/Scanner/Symbols.java
+++ src/main/java/Scanner/Symbols.java
@@ -2,14 +2,26 @@ package Scanner;
 
 public enum Symbols {
 
-    IDENT   (0),
-    NUMBER  (1),
-    LPAREN  (2),
-    RPAREN  (3),
-    PLUS    (4),
-    MINUS   (5),
-    TIMES   (6),
-    OTHER   (99);
+    IDENT       (0),        // ident
+    NUMBER      (1),        // number
+    LPAREN      (2),        // (
+    RPAREN      (3),        // )
+    PLUS        (4),        // +
+    MINUS       (5),        // -
+    TIMES       (6),        // *
+    SLASH       (7),        // /
+    LBRACKET    (8),        // [
+    RBRACKET    (9),        // ]
+    COMMA       (10),       // ,
+    SEMI        (11),       // ;
+    ASSIGN      (12),       // =
+    EQUAL       (13),       // ==
+    NEQUAL      (14),       // !=
+    GREATER     (15),       // >
+    SMALLER     (16),       // <
+    GR_EQ       (17),       // >=
+    SM_EQ       (18),       // <=
+    OTHER       (99);
 
 
     public final int symbol;
