070a45fefe464259bb5666355e6e5719ddc37999
Kr3b5 <codersuniverse42@gmail.com>
2021-11-18 14:44:06 +0100

    Add error cases

--- src/test/java/Scanner/ParserTests.java
+++ src/test/java/Scanner/ParserTests.java
@@ -8,26 +8,39 @@ import java.io.FileNotFoundException;
 public class ParserTests {
 
     // normal cases
-    private static final String COMPLETE_TEST = "./src/test/resources/pass_test/CompleteTest.jsst";
-    private static final String EMPTY_CLASS_TEST = "./src/test/resources/pass_test/EmptyClass.jsst";
+    private static final String COMPLETE_TEST           = "./src/test/resources/pass_test/CompleteTest.jsst";
+    private static final String EMPTY_CLASS_TEST        = "./src/test/resources/pass_test/EmptyClass.jsst";
 
     // error cases
-    private static final String BRACKET_MISSING_TEST = "./src/test/resources/error_case/Error_MissingClassBracket.jsst";
+    private static final String E_MISSING_BRACKET_IF    = "./src/test/resources/error_case/Error_MissingBracketIf.jsst";
+    private static final String E_MISSING_BRACKET_CLASS = "./src/test/resources/error_case/Error_MissingClassBracket.jsst";
+    private static final String E_MISSING_ELSE          = "./src/test/resources/error_case/Error_MissingElse.jsst";
+    private static final String E_MISSING_SEMI_METHOD   = "./src/test/resources/error_case/Error_MissingSemicolonInMethod.jsst";
+    private static final String E_MISSING_STATEMENT     = "./src/test/resources/error_case/Error_MissingStatement.jsst";
+
+
+    // normal tests
+    @Test
+    public void NormalTest_complete(){ runOverFile(COMPLETE_TEST); }
 
     @Test
-    public void NormalTest_complete(){
-        runOverFile(COMPLETE_TEST);
-    }
+    public void NormalTest_EmptyClass(){ runOverFile(EMPTY_CLASS_TEST); }
+
+    // error tests
+    @Test
+    public void ErrorTest_MissBra_If(){ runOverFile(E_MISSING_BRACKET_IF); }
 
     @Test
-    public void NormalTest_EmptyClass(){
-        runOverFile(EMPTY_CLASS_TEST);
-    }
+    public void ErrorTest_MissBra_Class(){ runOverFile(E_MISSING_BRACKET_CLASS); }
 
     @Test
-    public void ErrorTest_BracketMissing(){
-        runOverFile(BRACKET_MISSING_TEST);
-    }
+    public void ErrorTest_MissElse_If(){ runOverFile(E_MISSING_ELSE); }
+
+    @Test
+    public void ErrorTest_MissSemi(){ runOverFile(E_MISSING_SEMI_METHOD); }
+
+    @Test
+    public void ErrorTest_MissStatement(){ runOverFile(E_MISSING_STATEMENT); }
 
     private void runOverFile(String path){
         try {
